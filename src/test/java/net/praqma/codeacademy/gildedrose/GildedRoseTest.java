2cbcaeb7fe4fd43605a46a6b59ddc73c6fa19a1a
Thomas Lin Pedersen <thomasp85@gmail.com>
2017-12-05 13:56:12 +0000

    my first commit

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GildedRoseTest {
-
+// comment - yeah
     @Test
     public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
